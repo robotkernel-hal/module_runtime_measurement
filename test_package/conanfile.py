1db61c889f1139b5f5bb3597d76d9df626c0b44f
Robert Burger <robert.burger@dlr.de>
2026-03-16 10:59:48 +0100

    fix: copy with same src und dest

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,21 +1,21 @@
 import os
 import shutil
 
-from conans import ConanFile, CMake, tools
-from conans.client.run_environment import RunEnvironment
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.files import copy
 
 class module_runtime_measurement_test(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     exports_sources = "mod_test.rkc"
+    generators = "VirtualRunEnv"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def test(self):
-        rkc_path = os.path.join(self.source_folder, "mod_test.rkc")
-        shutil.copy(rkc_path, 'mod_test.rkc')
-
-        if not tools.cross_building(self.settings):
-            re = RunEnvironment(self)
-            with tools.environment_append(re.vars):
-                self.run("robotkernel --test-run --config .%smod_test.rkc" % os.sep)
+        if can_run(self):
+            self.run("robotkernel --test-run --config %s" % os.path.join(self.package_folder, "mod_test.rkc"), env="conanrun")
         else:
             self.output.warn("Skipping run cross built package")
 
