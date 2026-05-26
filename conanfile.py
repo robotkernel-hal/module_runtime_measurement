434c78e2cb72729c9259c901b19ad90101a88f79
Robert Burger <robert.burger@dlr.de>
2026-05-26 10:30:16 +0200

    change: using stable deps

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,13 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[^5.0.6]@robotkernel/stable"
+    python_requires = "conan_template/[~6]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_runtime_measurement"
     description = "robotkernel runtime measurement module."
     exports_sources = ["*", "!.gitignore"]
-    requires = ["robotkernel/[~6]@robotkernel/unstable", "service_provider_process_data_inspection/[~6]@robotkernel/unstable", ]
+    requires = ["robotkernel/[~6]@robotkernel/stable", "service_provider_process_data_inspection/[~6]@robotkernel/stable", ]
     
     def source(self):
         self.run(f"sed 's/AC_INIT(.*/AC_INIT([{self.name}], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
